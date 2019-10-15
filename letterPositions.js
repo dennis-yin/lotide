f0e1df8030ad1e23c50269a084206abc000180d5
dennis-yin <dennis.yin26@gmail.com>
2019-10-15 21:23:18 +0000

    adding function to count the index of each letter occurence in a string

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log("Assertion Passed: The two arrays are equal.")
+  } else {
+    console.log("Assertion Failed; The two arrays are not equal.")
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter === " ") {
+      continue;
+    } else {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [];
+        results[letter].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello")["h"], [0]);
+assertArraysEqual(letterPositions("hello")["e"], [1]);
+assertArraysEqual(letterPositions("hello")["l"], [2,3]);
+assertArraysEqual(letterPositions("hello")["o"], [4]);
