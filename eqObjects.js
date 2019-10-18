7f117f434a8774135accbda7e3e895d96a94a0bb
dennis-yin <dennis.yin26@gmail.com>
2019-10-18 23:21:53 +0000

    adding a recursive way to check if two objects are equal to eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -30,6 +30,9 @@ const eqObjects = function(object1, object2) {
   for (key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
+    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object'
+              && object1[key] !== null && object2[key] !== null) {
+      return eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
     }
@@ -38,9 +41,6 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+assertEqual(eqObjects({ a: { y: 0, z: { c: 3 } }, b: 2 }, { a: { y: 0, z: { c: 3 } }, b: 2 }), true) // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
